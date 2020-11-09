c78d9a39204b14e7de30798b5d4babd8bc138150
Isabelle Foster <isfoster96@gmail.com>
2020-11-08 22:11:26 -0500

    Update data

--- scripts/update_site.R
+++ scripts/update_site.R
@@ -1,17 +1,22 @@
 # Update site.
 
 # Authors: Sara Altman, Bill Behrman
-# Version: 2020-06-16
+# Version: 2020-11-04
 
 # Parameters
+  # Script to generate site RMDs
+script_build <- here::here("R/build.R")
   # Directory for site
 dir_site <- here::here("docs")
 
 #===============================================================================
 
+Sys.time()
+
 cli::cat_line(cli::rule("Building site"))
 options(blogdown.publishDir = dir_site)
-blogdown::build_site(local = TRUE)
+source(script_build)
+blogdown::build_site(local = TRUE, build_rmd = TRUE)
 
 cli::cat_line(cli::rule("Pushing site to GitHub"))
 cli::cat_line(cli::rule("    Adding"))
@@ -20,3 +25,5 @@ cli::cat_line(cli::rule("    Committing"))
 system("git commit -m 'Update site'")
 cli::cat_line(cli::rule("    Pushing"))
 system("git push")
+
+Sys.time()
